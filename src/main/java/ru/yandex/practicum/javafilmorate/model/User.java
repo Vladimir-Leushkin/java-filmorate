a3cd8ca2abc4a9b79473232b43f9c0b6f71f6fa9
Владимир <Vleyshkin@yandex.ru>
2022-07-09 14:38:03 +0300

    fix: добавить новые поля

--- src/main/java/ru/yandex/practicum/javafilmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/User.java
@@ -1,8 +1,11 @@
 package ru.yandex.practicum.javafilmorate.model;
 
 import lombok.Data;
+import org.springframework.web.servlet.tags.form.SelectTag;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class User {
@@ -11,4 +14,9 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+    private Set<Integer> friends = new HashSet<>();
+
+    public void addFriend(Integer id) {
+        friends.add(id);
+    }
 }
